cfaee304152f5221edab43ead5160f6346a42380
alex1779 <alex1779@gmail.com>
2023-10-21 18:32:19 -0300

    Adding new style and icons to navbar for companies

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+import { BsFillMicFill, BsFillGearFill } from 'react-icons/bs';
 import styles from '../styles/Navbar.module.css';
 
 function Navbar() {
@@ -5,8 +6,26 @@ function Navbar() {
 
     <header className={styles.header}>
       <div>
+        <p className={styles.year}>2023</p>
+      </div>
+
+      <div>
         <h1 className={styles.title}>Financial Modeling Prep</h1>
       </div>
+
+      <button type="button">
+        <BsFillMicFill
+          color="white"
+          size="5vw"
+        />
+      </button>
+
+      <button type="button">
+        <BsFillGearFill
+          color="white"
+          size="5vw"
+        />
+      </button>
     </header>
 
   );
